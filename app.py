11131fa4e0841a49638476f2f178307227bd1e31
kennyh99 <58242500+kennyh99@users.noreply.github.com>
2020-12-12 20:15:54 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+from hotel import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ hotel/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = 'hello_world'
+app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+
+from hotel import models
+from hotel import views
+
